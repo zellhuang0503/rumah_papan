215d4b668640deb39135d7d847a253a2bd076ae9
zellhuang0503 <zell.huang@gmail.com>
2026-01-21 16:49:52 +0800

    fix: add hardcoded fallback for sanity project id

--- src/utils/sanity.ts
+++ src/utils/sanity.ts
@@ -2,8 +2,8 @@ import { createClient } from '@sanity/client';
 import imageUrlBuilder from '@sanity/image-url';
 
 export const client = createClient({
-    projectId: import.meta.env.VITE_SANITY_PROJECT_ID,
-    dataset: import.meta.env.VITE_SANITY_DATASET,
+    projectId: import.meta.env.VITE_SANITY_PROJECT_ID || 'pt01rhvf',
+    dataset: import.meta.env.VITE_SANITY_DATASET || 'production',
     apiVersion: import.meta.env.VITE_SANITY_API_VERSION || '2024-03-24',
     useCdn: false, // Set to false to ensure we get fresh data from Sanity immediately after publishing
 });
